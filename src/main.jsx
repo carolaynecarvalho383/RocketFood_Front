a8df1232518a518cad7d70525090a610c87ce2b1
Luis Murilo <luiszkm@gmail.com>
2022-08-23 23:27:07 -0300

    add auth context

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 
 import { Routes } from './routes'
 
+import { AuthProvider } from './hooks/auth'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from './styles/global'
 import theme from './styles/theme'
@@ -12,7 +13,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 )
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,5 +1,7 @@
-
+import { useState } from "react";
 import { Link } from "react-router-dom"
+
+import { useAuth } from "../../hooks/auth"
 
 import { Input } from "../../components/Input";
 import { Logo } from "../../components/Logo";
@@ -9,22 +11,37 @@ import { Button } from "../../components/Button";
 import { Container, Form } from "./styles";
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  const { signIn } = useAuth()
+
+  function handleSignIn(e) {
+    e.preventDefault()
+    signIn({email, password})
+  }
+
 
   return (
     <Container>
-      <Logo title="Food Explorer" className ="main-logo"/>
+      <Logo title="Food Explorer" className="main-logo" />
       <Section title=" Faça Login">
 
         <Form >
           <Input name="email"
-            placeholder="exemplo@exemplo.com" />
+            placeholder="exemplo@exemplo.com"
+            onChange={e => setEmail(e.target.value)} />
 
           <Input name="senha"
             type="password"
-            placeholder="No mínimo 6 caracteres" />
+            placeholder="No mínimo 6 caracteres"
+            onChange={e => setPassword(e.target.value)} />
 
-          <Button title=" Entrar" />
-          <Link to= "/register">Criar Conta</Link>
+          <Button title=" Entrar"
+            onClick={handleSignIn} />
+
+          <Link to="/register">Criar Conta</Link>
         </Form>
       </Section>
 
