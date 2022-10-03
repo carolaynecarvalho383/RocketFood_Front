37f62d885eefad32b9dc6e9b10fed278cc9b2866
luis <luiszkm@gmail.com>
2022-10-03 15:25:04 -0300

    add ingredient image

--- src/components/IngredientsContainer/index.jsx
+++ src/components/IngredientsContainer/index.jsx
@@ -1,40 +1,19 @@
 
-import arugula from "../../assets/ingredients/arugula.svg";
-import bread_nan from "../../assets/ingredients/bread_nan.svg";
-import bread from "../../assets/ingredients/bread.svg";
-import cucumber from "../../assets/ingredients/cucumber.svg";
-import ham from "../../assets/ingredients/ham.svg";
-import lettuce from "../../assets/ingredients/lettuce.svg";
-import pasta from "../../assets/ingredients/pasta.svg";
-import radish from "../../assets/ingredients/radish.svg";
-import shrimp from "../../assets/ingredients/shrimp.svg";
-import tomato from "../../assets/ingredients/tomato.svg";
-import plum from "../../assets/ingredients/plum.svg";
-import flour from "../../assets/ingredients/flour.svg";
-import peach from "../../assets/ingredients/peach.svg";
-import almonds from "../../assets/ingredients/almonds.svg";
-import eggWhite from "../../assets/ingredients/eggWhite.svg";
-import apricot from "../../assets/ingredients/apricot.svg";
-import cafe from "../../assets/ingredients/cafe.svg";
-import passionFruit from "../../assets/ingredients/passionFruit.svg";
-import cinnamon from "../../assets/ingredients/cinnamon.svg";
-import anise from "../../assets/ingredients/anise.svg";
-import lemon from "../../assets/ingredients/lemon.svg";
-import apple from "../../assets/ingredients/apple.svg";
-import whiskey from "../../assets/ingredients/whiskey.svg";
+
 
 import { Container } from "./styles";
 import { useEffect } from "react";
 import { api } from "../../services/api";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
+import { IngredientImg } from "../IngredientImg";
 
-const ingredients = [cafe]
 
 export function IngredientsContainer() {
   const [data, setData] = useState([])
   const params = useParams()
 
+  console.log(data);
   useEffect(() => {
     async function fetchIngredients() {
       const response = await api.get(`/ingredients/${params.id}/`)
@@ -45,13 +24,14 @@ export function IngredientsContainer() {
     <Container>
       {data &&
         data.map(ingredient => (
-          <div>
-            <img src={ingredients.filter(name => {
-              const image = name.toLowerCase().indexOf(`/src/assets/ingredients/${ingredient.ingredientName}.svg`) !== -1
-              return image
-            })} alt={`imagem de ${ingredient.ingredientName}`} />
-            <span>{ingredient.ingredientName}</span>
-          </div>
+          /*  <div>
+              <img src={ingredients.filter(name => {
+                const image = name.toLowerCase().indexOf(`/src/assets/ingredients/${ingredient.ingredientName}.svg`) !== -1
+                return image
+              })} alt={`imagem de ${ingredient.ingredientName}`} />
+              <span>{ingredient.ingredientName}</span>
+            </div>*/
+          <IngredientImg ingredientName={ingredient.ingredientName} />
         ))}
     </Container>
   )
